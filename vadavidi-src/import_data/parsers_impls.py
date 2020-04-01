bd9abe6d59147ab5f3f74c9e6b35e0965c6bf266
martlin2cz2 <martlin@seznam.cz>
2020-04-01 21:51:53 +0200

    Implemented CSV parser + Excel CSV parser
     - yea
     - implemented general CSV parser (using the library)
     - and the CSV parser for excel files

--- vadavidi-src/import_data/parsers_impls.py
+++ vadavidi-src/import_data/parsers_impls.py
@@ -1,10 +1,13 @@
 # The base module for parsers
 
+import csv
+
 from datas import Schema, Entry, Table
-from base_parsers import LinesSplittingParser
+from base_parsers import LinesSplittingParser, IteratingParser
 from datas_util import MutableTable
 
 
+########################################################################
 ########################################################################
 # The simple CSV parser.
 # Assumes values on lines separated with one simple separator,
@@ -21,11 +24,76 @@ class SimpleCSVParser(LinesSplittingParser):
 		return Entry(ordnum, values)	
 
 ########################################################################
+########################################################################
+# The (proper) CSV files parser. Allows to specify the format.
+class CSVParser(IteratingParser):
+	# the csv input format configuration
+	delimiter = ','
+	doublequote = 1
+	escapechar = '\\'
+	lineterminator = '\n'
+	quotechar = '"'
+	skipinitialspace = 1
+	strict = 1
+	
+	# converts the input file to list of fractions
+	def fracte(self, schema, file_name):
+		with open(file_name, newline='') as csvfile:
+			reader = csv.reader(csvfile, 
+								delimiter = self.delimiter,
+								doublequote = self.doublequote,
+								escapechar = self.escapechar,
+								lineterminator = self.lineterminator,
+								quotechar = self.quotechar,
+								skipinitialspace = self.skipinitialspace,
+								strict = self.strict)
+			return list(reader)
+		
+	# converts given fraction to entry
+	def parseFraction(self, ordnum, fraction, schema):
+		values = dict(list(map(
+						lambda fn,val: (fn, val),
+						schema.listFieldNames(), fraction)));		
+		return Entry(ordnum, values)
+	
+########################################################################
+########################################################################
+# The Excel "CSV" files parser. Uses the default excel format.
+class ExcelCSVParser(CSVParser):
+	# the csv input format configuration
+	delimiter = ','
+	doublequote = 1
+	escapechar = None
+	lineterminator = '\r\n'
+	quotechar = '"'
+	skipinitialspace = 0
+	strict = 0
+		
+########################################################################
+########################################################################
 if __name__== "__main__":
+	schema = Schema({"first": "str", "number": "int", "second": "str"})
+	
 	print("Runining simple CSV parser")
 	parser = SimpleCSVParser()
 	input_file = "../testdata/first.csv"
-	schema = Schema({"first": "str", "number": "int", "second": "str"})
+	
+	table = parser.parse(schema, input_file)
+	print(table)
+	table.printit()
+	
+	print("Runining CSV parser")
+	parser = CSVParser()
+	parser.delimiter = ';'
+	input_file = "../testdata/second.csv"
+	
+	table = parser.parse(schema, input_file)
+	print(table)
+	table.printit()
+	
+	print("Runining Excel CSV parser")
+	parser = ExcelCSVParser()
+	input_file = "../testdata/third.csv"
 	
 	table = parser.parse(schema, input_file)
 	print(table)
